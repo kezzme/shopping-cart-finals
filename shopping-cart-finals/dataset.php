e22b0daa7aeb9a476fa43380b78a298043e2e9eb
kezzme <la.torre.troy09@gmail.com>
2022-11-25 11:05:29 +0800

    Initial Commit

---
+++ shopping-cart-finals/dataset.php
@@ -0,0 +1,60 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "Bayaband Clog in Black White",
+            'description'   => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dignissim vulputate tortor, sed ornare dui pharetra sed. Aliquam posuere eleifend eros nec aliquam. Aliquam ac risus a tellus mollis ultrices. Mauris tellus felis, luctus id metus nec, tristique molestie neque.",
+            'price'         => "2999",
+            'photo1'        => "BCBW.png",
+            'photo2'        => "BCBW1.png",
+        ),
+        array(
+            'name'          => "Bayaband Slide in Navy Pepper",
+            'description'   => "Aenean tincidunt mattis eros, non venenatis felis suscipit eu. Fusce a tempus nulla. Cras pretium purus augue, ut semper arcu pulvinar sit amet. Nam et est consectetur, mollis mi nec, pharetra nibh. Sed malesuada pellentesque ipsum, vel congue purus. Mauris eget velit vel metus ullamcorper lacinia. Sed in vestibulum erat.",
+            'price'         => "1745",
+            'photo1'        => "BSNP.png",
+            'photo2'        => "BSNP1.png",
+        ),
+        array(
+            'name'          => "Classic Clog in Black",
+            'description'   => "Donec ac neque non turpis aliquet vehicula a in eros. Vestibulum ante mauris, condimentum nec ex eget, ultrices consequat felis. Curabitur nec consequat magna. Donec euismod magna tortor, eget ultrices mauris lobortis et. Etiam imperdiet aliquet porta. Maecenas magna felis, placerat et rhoncus quis, gravida in nunc.",
+            'price'         => "3245",
+            'photo1'        => "CCB.png",
+            'photo2'        => "CCB1.png",
+        ),
+        array(
+            'name'          => "Classic Translucent Solarized Clog in Black Multi",
+            'description'   => "Proin rutrum volutpat ultricies. In vitae lorem vitae augue blandit lobortis. Aenean vel elit malesuada, semper diam et, tristique quam. Praesent scelerisque ipsum vitae vehicula aliquet. Morbi odio risus, consectetur in tristique eget, scelerisque nec erat. Nulla porttitor erat eget risus scelerisque, vitae gravida tortor interdum. Aliquam commodo lacinia ante sit amet commodo.",
+            'price'         => "1999",
+            'photo1'        => "CTSC.png",
+            'photo2'        => "CTSC1.png",
+        ),
+        array(
+            'name'          => "Literide 360 Clog in Black Slate Grey",
+            'description'   => "Nullam pharetra sit amet lectus eu mattis. Cras nec blandit massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur volutpat, velit eu bibendum aliquet, justo justo sodales ligula, sit amet fermentum velit est id sapien. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras eget scelerisque dui.",
+            'price'         => "2999",
+            'photo1'        => "L360.png",
+            'photo2'        => "L3601.png",
+        ),
+        array(
+            'name'          => "Classic Sandal in Navy",
+            'description'   => "Maecenas gravida viverra felis, in rutrum dolor. Nam pharetra mollis urna, eu sagittis odio luctus quis. Pellentesque vulputate arcu purus, non tincidunt ligula rutrum ac. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc et nibh sed massa vulputate sagittis. Fusce sit amet odio ut dui auctor venenatis eu non leo. ",
+            'price'         => "1399",
+            'photo1'        => "CSN.png",
+            'photo2'        => "CSN1.png",
+        ),
+        array(
+            'name'          => "Literide 360 Clog in Navy Blue Grey",
+            'description'   => "Morbi pharetra nisi felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse pretium sem ut orci semper fermentum. Curabitur quis dui quis quam laoreet sollicitudin. Quisque sit amet faucibus ligula, quis pharetra lacus. Proin ut imperdiet lectus, sit amet placerat risus.",
+            'price'         => "2999",
+            'photo1'        => "LCNB.png",
+            'photo2'        => "LCNB1.png",
+        ), 
+        array(
+            'name'          => "Classic Clog in Bone",
+            'description'   => "Donec rutrum, ipsum ac molestie tristique, nisi leo hendrerit nibh, eu volutpat augue nibh in massa. Nulla molestie elit sit amet urna mattis, molestie pharetra tortor venenatis. In quis dolor sed urna maximus dignissim. Morbi aliquet nulla eget erat accumsan, id pretium ex varius.",
+            'price'         => "3245",
+            'photo1'        => "CCBone.png",
+            'photo2'        => "CCBone1.png",
+        )
+    );
+?>
